534025b54ccb5b3d4c2f29161b1d2878589bfcd9
WilliamEsc <escriva.william@outlook.com>
2022-03-19 22:43:18 +0100

    :sparkles: task_1 end

--- src/aircraftFactory.cpp
+++ src/aircraftFactory.cpp
@@ -1,8 +1,22 @@
 #include "aircraftFactory.hpp"
 
-std::unique_ptr<Aircraft> AircraftFactory::create_aircraft(const AircraftType& type, const Tower& tower) const
+
+void AircraftFactory::init_aircraft_types(){
+    aircraft_types[0] = new AircraftType { .02f, .05f, .02f, MediaPath { "l1011_48px.png" } };
+    aircraft_types[1] = new AircraftType { .02f, .05f, .02f, MediaPath { "b707_jat.png" } };
+    aircraft_types[2] = new AircraftType { .02f, .08f, .03f, MediaPath { "concorde_af.png" } };
+}
+
+std::unique_ptr<Aircraft> AircraftFactory::create_aircraft(const AircraftType& type, Tower& tower)
 {
-    const std::string flight_number = airlines[std::rand() % 8] + std::to_string(1000 + (rand() % 9000));
+    std::cout<< airflight_number_set.size();
+    std::string number = airlines[std::rand() % 8] + std::to_string(1000 + (rand() % 9000));
+    while(airflight_number_set.find(number) != airflight_number_set.end()){
+        number = airlines[std::rand() % 8] + std::to_string(1000 + (rand() % 9000));
+    }
+    const std::string flight_number = number;
+    airflight_number_set.emplace(flight_number);
+    std::cout<< " after " << airflight_number_set.size() <<std::endl;
     const float angle       = (rand() % 1000) * 2 * 3.141592f / 1000.f; // random angle between 0 and 2pi
     const Point3D start     = Point3D { std::sin(angle), std::cos(angle), 0 } * 3 + Point3D { 0, 0, 2 };
     const Point3D direction = (-start).normalize();
@@ -10,7 +24,7 @@ std::unique_ptr<Aircraft> AircraftFactory::create_aircraft(const AircraftType& t
     return std::make_unique<Aircraft>(type, flight_number, start, direction, tower);
 }
 
-std::unique_ptr<Aircraft> AircraftFactory::create_random_aircraft(const Tower& tower) const
+std::unique_ptr<Aircraft> AircraftFactory::create_random_aircraft(Tower& tower)
 {
     return create_aircraft(*(aircraft_types[rand() % 3]), tower);
 }
--- src/aircraftFactory.hpp
+++ src/aircraftFactory.hpp
@@ -4,17 +4,18 @@
 #include "aircraft_types.hpp"
 
 #include <string>
+#include <set>
 
 class AircraftFactory
 {
 private:
+    std::set<std::string> airflight_number_set = {};
     std::string airlines[8] = { "AF", "LH", "EY", "DL", "KL", "BA", "AY", "EY" };
-    AircraftType* aircraft_types[3] { new AircraftType { .02f, .05f, .02f, MediaPath { "l1011_48px.png" } },
-                                      new AircraftType { .02f, .05f, .02f, MediaPath { "b707_jat.png" } },
-                                      new AircraftType { .02f, .08f, .02f,
-                                                         MediaPath { "concorde_af.png" } } };
+    AircraftType* aircraft_types[3] {};
 
 public:
-    std::unique_ptr<Aircraft> create_aircraft(const AircraftType& type, const Tower& tower) const;
-    std::unique_ptr<Aircraft> create_random_aircraft(const Tower& tower) const;
+
+    void init_aircraft_types();
+    std::unique_ptr<Aircraft> create_aircraft(const AircraftType& type, Tower& tower);
+    std::unique_ptr<Aircraft> create_random_aircraft(Tower& tower);
 };
